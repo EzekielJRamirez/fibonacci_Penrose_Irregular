48295ed0888594589a59f446d045c9931efacd70
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-11-09 10:09:35 -0800

    added empty methods

--- src/main.java
+++ src/main.java
@@ -4,4 +4,16 @@
  */
 
 public class main {
+    public static void main(String[] args) {
+        //start adding psuedo code
+    }
+
+    public static void latitude() {}
+
+    public static void longitude() {}
+
+    public static void genX() {}
+    public static void genY() {}
+    public static void genZ() {}
+    public static void genCoord() {}
 }
