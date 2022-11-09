80722decb332f3cb1a308aba18d91139f6da92bf
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-11-09 10:10:46 -0800

    made sections to write comments

--- src/main.java
+++ src/main.java
@@ -8,12 +8,33 @@ public class main {
         //start adding psuedo code
     }
 
+    /**
+     *
+     */
     public static void latitude() {}
 
+    /**
+     *
+     */
     public static void longitude() {}
 
+    /**
+     *
+     */
     public static void genX() {}
+
+    /**
+     *
+     */
     public static void genY() {}
+
+    /**
+     *
+     */
     public static void genZ() {}
+
+    /**
+     *
+     */
     public static void genCoord() {}
 }
