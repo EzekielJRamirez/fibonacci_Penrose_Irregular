8d187debdc57faddbf10c66746a704efc93bb16f
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-11-09 22:15:56 -0800

    Worked on the first two methods

--- src/main.java
+++ src/main.java
@@ -6,35 +6,75 @@
 public class main {
     public static void main(String[] args) {
         //start adding psuedo code
+
+//        double lat = latitude(2, 5);
+
+//        System.out.println(lat);
+
+        double frac = (2 * 2.0) / (2 * 5.0 + 1);
+        System.out.println(frac);
+        double fracSine = Math.asin(frac);
+        System.out.println(fracSine);
+
+//        double pie = Math.PI - 1;
+//        System.out.println(pie);
+    }
+
+    /**
+     * Math.PI stores the constant up to the 15th decimal place
+     * Math.PI is a final variable that doesn't update, so
+     * we will emulate it.
+     *
+     * there is an issue with how math.asin calculates my fraction
+     * it may be due to the vars i and n being integers
+     *
+     * to get Math.asin() to work I may need to convert some of these values
+     * radians and maybe convert them back
+     */
+    public static double latitude(int i, int n) {
+        System.out.println(Math.PI);
+        double pie = 3.141592653589793;
+
+        double ret = pie - Math.asin((2.0 * i) / (2.0 * n + 1));
+        double ret2 = pie - 1;
+        System.out.println(Math.asin(((2.0 * i) / (2.0 * n + 1))));
+
+        if (i <= 0 ) {
+            return Math.asin((2.0 * i) / (2 * n + 1));
+        } else {
+            return (pie - Math.asin((2.0 * i) / (2 * n + 1)));
+        }
     }
 
     /**
      *
      */
-    public static void latitude() {}
+    public static void longitude(int i) {
+        double pie = Math.PI;
+        double some = Math.
+    }
 
     /**
      *
      */
-    public static void longitude() {}
+    public static void genX() {
+    }
 
     /**
      *
      */
-    public static void genX() {}
+    public static void genY() {
+    }
 
     /**
      *
      */
-    public static void genY() {}
+    public static void genZ() {
+    }
 
     /**
      *
      */
-    public static void genZ() {}
-
-    /**
-     *
-     */
-    public static void genCoord() {}
+    public static void genCoord() {
+    }
 }
