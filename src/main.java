bcc37b0daae92acc345c9238bf534765dff0e3fe
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-11-10 09:34:21 -0800

    speak with Sanders for some intuition behind some of these arrays

--- src/main.java
+++ src/main.java
@@ -1,47 +1,35 @@
 /**
  * Team 1
  * Ezekiel Ramirez
- *
+ * <p>
  * pie 3.14159265359
  * phi 1.61803398875
  */
 
 public class main {
     public static void main(String[] args) {
-        //start adding psuedo code
-
 //        double lat = latitude(2, 5);
 
-//        System.out.println(lat);
-
-        double frac = (2 * 2.0) / (2 * 5.0 + 1);
-        System.out.println(frac);
-        double fracSine = Math.asin(frac);
-        System.out.println(fracSine);
-
-//        double pie = Math.PI - 1;
-//        System.out.println(pie);
-
-        System.out.println(longitude(20));
+        // arcsine assumes radian values are input into its formula
+        System.out.println(latitude(4, 5));
+        // these given values should give a value of 0.814
+//        System.out.println(Math.asin((2.0 * 4) / (2 * 5 + 1)));
     }
 
     /**
-     * TODO this method needs work!
-     *
+     * Math.asin() assumes radian values from its inputs
+     * <p>
      * Math.PI stores the constant up to the 15th decimal place
      * Math.PI is a final variable that doesn't update, so
      * we will emulate it.
-     *
+     * <p>
      * there is an issue with how math.asin calculates my fraction
      * it may be due to the vars i and n being integers
-     *
-     * to get Math.asin() to work I may need to convert some of these values
-     * radians and maybe convert them back
      */
     public static double latitude(int i, int n) {
         double pie = Math.PI;
 
-        if (i <= 0 ) {
+        if (i <= 0) {
             return Math.asin((2.0 * i) / (2 * n + 1));
         } else {
             return (pie - Math.asin((2.0 * i) / (2 * n + 1)));
@@ -50,7 +38,7 @@ public class main {
 
     /**
      * THIS METHOD WORKS
-     *
+     * <p>
      * phi is considered a golden ratio and appears naturally
      * in the world
      */
@@ -58,7 +46,7 @@ public class main {
         double pie = Math.PI;
         double phi = 1.61803398875;
 
-        return ((2 * pie * i)/phi);
+        return ((2 * pie * i) / phi);
     }
 
     /**
@@ -70,8 +58,8 @@ public class main {
         double[] a = new double[]{};
         int i = -n;
 
-        while (i <= n){
-            a[i] += (-r + Math.cos(latitude(i,n)) * Math.cos(longitude(i)));
+        while (i <= n) {
+            a[i] += (-r + Math.cos(latitude(i, n)) * Math.cos(longitude(i)));
             i += 1;
         }
 
@@ -81,18 +69,18 @@ public class main {
     /**
      *
      */
-    public static void genY() {
+    public static void genY(int n, double r) {
     }
 
     /**
      *
      */
-    public static void genZ() {
+    public static void genZ(int n, double r) {
     }
 
     /**
      *
      */
-    public static void genCoord() {
+    public static void genCoord(int n, double r) {
     }
 }
